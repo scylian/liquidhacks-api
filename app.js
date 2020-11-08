e574126f749887229c8eddd393b2529075be2174
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-08 13:32:38 -0600

    added routes and actual query

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const user = new Twitter({
 });
 
 
-app.get('/', async (req, res) => {
+app.get('/blue', async (req, res) => {
   const response = await user.getBearerToken();
   const client = new Twitter({
     bearer_token: response.access_token
   });
   const results = await client.get('search/tweets', {
-    q: "(@TeamLiquid)"
+    q: "(@TeamLiquid) #TeamBlue"
   });
 
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,7 +24,40 @@ app.get('/', async (req, res) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   res.json(results);
-  // res.send('Home Page');
+});
+
+app.get('/gold', async (req, res) => {
+  const response = await user.getBearerToken();
+  const client = new Twitter({
+    bearer_token: response.access_token
+  });
+  const results = await client.get('search/tweets', {
+    q: "(@TeamLiquid) #TeamGold"
+  });
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, ');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  res.json(results);
+});
+
+app.get('/white', async (req, res) => {
+  const response = await user.getBearerToken();
+  const client = new Twitter({
+    bearer_token: response.access_token
+  });
+  const results = await client.get('search/tweets', {
+    q: "(@TeamLiquid) #TeamWhite"
+  });
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, ');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  res.json(results);
 });
 
 const port = process.env.port || 3000;
