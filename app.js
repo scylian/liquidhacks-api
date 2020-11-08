0a01623642173321a1c05944fa1eac0961086ca2
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-08 12:09:31 -0600

    for frontend testing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
     bearer_token: response.access_token
   });
   const results = await client.get('search/tweets', {
-    q: "(@TeamLiquid) #ClashRoyale"
+    q: "(@TeamLiquid)"
   });
 
   res.json(results);
