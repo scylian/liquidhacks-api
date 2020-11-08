7c7a89aabe82e7de19c146a7473dbcd7f9b10ced
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-08 06:25:01 -0600

    fix port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
-app.listen(8000, () =>{
+const port = process.env.port || 3000;
+app.listen(port, () =>{
   console.log('Server started');
 });
