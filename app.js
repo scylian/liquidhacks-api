a25e28eee090b3c5e2dda0413a479d1bdaecf7e5
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-08 12:34:42 -0600

    fixed typo

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.get('/', async (req, res) => {
     q: "(@TeamLiquid)"
   });
 
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)OPTIONS
-  res.header('Access-Control-Allow-Methods', 'POST, GET, ')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, ');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   res.json(results);
   // res.send('Home Page');
