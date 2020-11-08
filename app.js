7685b6015838b169b85e48288b213472984955c7
Bien Nguyen <bien.g.nguyen@gmail.com>
2020-11-08 06:48:20 -0600

    try twitter api

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home Page');
+const Twitter = require('twitter-lite');
+
+const user = new Twitter({
+  consumer_key: "k0tRyQ4QRS1k3pU4Y9SO6EDY4",
+  consumer_secret: "FSyq8bHtEAOMPMRx0BNr9FZk7KQGW9Gu1VvFMULRqqIthjZyOI"
+});
+
+
+app.get('/', async (req, res) => {
+  const response = await user.getBearerToken();
+  const client = new Twitter({
+    bearer_token: response.access_token
+  });
+  const results = await client.get('search/tweets', {
+    q: "#teamliquid"
+  });
+
+  res.json(results);
+  // res.send('Home Page');
 });
 
 const port = process.env.port || 3000;
